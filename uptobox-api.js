8c2da1cec694efa9661c4e5e2abb4e6da6399048
Victorien Berlot <victorien@berlot.ch>
2019-11-28 23:16:16 +0100

    Set some parameters mandatory

--- uptobox-api.js
+++ uptobox-api.js
@@ -12,157 +12,101 @@ const urls = {
     download: 'https://uptobox.com/api/link'
 }
 
-let addFile = ({url, xfss}) => axios.post(url, null, {
+let addFile = (url, xfss) => axios.post(url, null, {
     headers: {'Cookie': xfss},
     withCredentials: true
 });
 
-let getAccount = token => axios.get(`${urls.account}?token=${token}`);
+let getUserData = token => axios.get(`${urls.account}?token=${token}`);
 
-let setSSL = ({token, is_ssl}) => axios({
+let setSSL = (token, ssl) => axios({
     method: 'PATCH',
     url: urls.settings,
-    data: {
-        token: token,
-        ssl: is_ssl
-    }
+    data: {token, ssl}
 });
 
-let setDirectDL = ({token, is_direct}) => axios({
+let setDirectDL = (token, directDownload) => axios({
     method: 'PATCH',
     url: urls.settings,
-    data: {
-        token: token,
-        directDownload: is_direct
-    }
+    data: {token, directDownload}
 });
 
-let setSecurityLock = ({token, is_locked}) => axios({
+let setSecurityLock = (token, securityLock) => axios({
     method: 'PATCH',
     url: urls.lock,
-    data: {
-        token: token,
-        securityLock: is_locked
-    }
+    data: {token, securityLock}
 });
 
-let convertPoints = ({token, points}) => axios({
+let convertPoints = (token, points) => axios({
     method: 'POST',
     url: urls.convert,
-    data: {
-        token: token,
-        points: points
-    }
+    data: {token, points}
 });
 
-let createVoucher = ({token, time, quantity}) => axios({
+let createVoucher = (token, time, quantity) => axios({
     method: 'POST',
     url: urls.voucher,
-    data: {
-        token: token,
-        time: time,
-        quantity: quantity
-    }
+    data: {token, time, quantity}
 });
 
 let getDownloadLink = options => axios.get(`${urls.download}?${querystring.stringify(options)}`);
 
-let listFiles = options => axios.get(`${urls.files}?${querystring.stringify(options)}`);
-
-let updateFileProperties = ({token, file_code, new_name, description, password, is_public}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        file_code: file_code,
-        new_name: new_name,
-        description: description,
-        password: password,
-        public: is_public
-    }
-});
-
-let updateFilesPublicProperty = ({token, file_codes, is_public}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        file_codes: file_codes,
-        public: is_public
-    }
-});
-
-let moveFolder = ({token, folder_id, new_parent_id}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        fld_id: folder_id,
-        destination_fld_id: new_parent_id,
-        action: 'move'
-    }
-});
-
-let moveFiles = ({token, file_codes, new_parent_id}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        file_codes: file_codes,
-        destination_fld_id: new_parent_id,
-        action: 'move'
-    }
-});
-
-let copyFiles = ({token, file_codes, new_parent_id}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        file_codes: file_codes,
-        destination_fld_id: new_parent_id,
-        action: 'copy'
-    }
-});
-
-let renameFolder = ({token, folder_id, new_name}) => axios({
-    method: 'PATCH',
-    url: urls.files,
-    data: {
-        token: token,
-        fld_id: folder_id,
-        new_name: new_name
-    }
-});
-
-let createFolder = ({token, path, name}) => axios({
-    method: 'PUT',
-    url: urls.files,
-    data: {
-        token: token,
-        path: path,
-        name: name
-    }
-});
-
-let deleteFiles = ({token, file_codes}) => axios({
-    method: 'DELETE',
-    url: urls.files,
-    data: {
-        token: token,
-        file_codes: file_codes
-    }
-});
-
-let deleteFolder = ({token, folder_id}) => axios({
-    method: 'DELETE',
-    url: urls.files,
-    data: {
-        token: token,
-        fld_id: folder_id
-    }
-});
-
 let getStreamingLink = options => axios.get(`${urls.stream}?${querystring.stringify(options)}`);
 
-module.exports = { addFile, getAccount, setSSL, setDirectDL, setSecurityLock, convertPoints, createVoucher, getDownloadLink, listFiles, updateFileProperties, updateFilesPublicProperty, moveFolder, moveFiles, copyFiles, renameFolder, createFolder, deleteFiles, deleteFolder, getStreamingLink };
+let listFiles = options => axios.get(`${urls.files}?${querystring.stringify(options)}`);
+
+let updateFileProperties = ({token, file_code, new_name, description, password, public}) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, file_code, new_name, description, password, public}
+});
+
+let updateFilesPublicProperty = (token, file_codes, public) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, file_codes, public}
+});
+
+let moveFolder = (token, fld_id, destination_fld_id) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, fld_id, destination_fld_id, action: 'move'}
+});
+
+let moveFiles = (token, file_codes, destination_fld_id) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, file_codes, destination_fld_id, action: 'move'}
+});
+
+let copyFiles = (token, file_codes, destination_fld_id) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, file_codes, destination_fld_id, action: 'copy'}
+});
+
+let renameFolder = (token, fld_id, new_name) => axios({
+    method: 'PATCH',
+    url: urls.files,
+    data: {token, fld_id, new_name}
+});
+
+let createFolder = (token, path, name) => axios({
+    method: 'PUT',
+    url: urls.files,
+    data: {token, path, name}
+});
+
+let deleteFiles = (token, file_codes) => axios({
+    method: 'DELETE',
+    url: urls.files,
+    data: {token, file_codes}
+});
+
+let deleteFolder = (token, fld_id) => axios({
+    method: 'DELETE',
+    url: urls.files,
+    data: {token, fld_id}
+});
+
+module.exports = { addFile, getUserData, setSSL, setDirectDL, setSecurityLock, convertPoints, createVoucher, getDownloadLink, listFiles, updateFileProperties, updateFilesPublicProperty, moveFolder, moveFiles, copyFiles, renameFolder, createFolder, deleteFiles, deleteFolder, getStreamingLink };
